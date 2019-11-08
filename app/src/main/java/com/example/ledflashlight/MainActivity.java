1dcb4e82efe9d51f90a6474525f69b404df434b7
miro.jurisic <mirojurisic@hotmail.com>
2019-11-08 23:07:45 +0800

    added sos button to flash morse code sos

--- app/src/main/java/com/example/ledflashlight/MainActivity.java
+++ app/src/main/java/com/example/ledflashlight/MainActivity.java
@@ -15,17 +15,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
     private CameraManager cameraManager;
     private String cameraId;
-    private TextView onOffSwitch;
+    private TextView onOffSwitch, sos;
     private Boolean isOn;
+    private int milliseconds = 300;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
         onOffSwitch = findViewById(R.id.button_on_off);
+        sos = findViewById(R.id.sos);
         isOn = false;
         Boolean isFlashAvailable = getApplicationContext().getPackageManager()
                 .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
@@ -46,15 +51,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 try {
                     if (isOn) {
+                        onOffSwitch.setText("Turn ON");
                         turnOffFlashLight();
                         isOn = false;
                     } else {
+                        onOffSwitch.setText("Turn OFF");
                         turnOnFlashLight();
                         isOn = true;
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+            }
+        });
+        sos.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                shortRepeat(3);
+                longRepeat(3);
+                shortRepeat(3);
             }
         });
     }
@@ -71,6 +86,36 @@ public class MainActivity extends AppCompatActivity {
         try {
                 cameraManager.setTorchMode(cameraId, false);
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void shortRepeat(int multiple){
+        try {
+            for(int i=0;i<multiple;i++){
+            turnOnFlashLight();
+            TimeUnit.MILLISECONDS.sleep(milliseconds);
+            turnOffFlashLight();
+            TimeUnit.MILLISECONDS.sleep(milliseconds);
+            }
+
+    }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public void longRepeat(int multiple){
+        try {
+            for(int i=0;i<multiple;i++){
+                turnOnFlashLight();
+                TimeUnit.SECONDS.sleep(1);
+                turnOffFlashLight();
+                TimeUnit.MILLISECONDS.sleep(milliseconds);
+            }
+
+        }
+        catch (InterruptedException e)
+        {
             e.printStackTrace();
         }
     }
